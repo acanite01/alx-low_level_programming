97ccc730606d190b5827a5aad8f4ae21933d3cbf
acanite01 <oceanofscience01@gmail.com>
2022-09-15 15:31:05 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/***
  * main - Entry point
  *
  * Description: FizzBuzz test
@@ -9,29 +9,29 @@
 
 int main(void)
 {
-int n;
+	int n;
 
-for (n = 1; n <= 100; n++)
-{
-if (n % 3 == 0 && n % 5 == 0)
-{
-printf("%s", "FizzBuzz");
-}
-else if (n % 3 == 0)
-{
-printf("%s", "Fizz");
-}
-else if (n % 5 == 0)
-{
-printf("%s", "Buzz");
-}
-else
-{
-printf("%i", n);
-}
-if (n != 100)
-printf(" ");
-}
-printf("\n");
-return (0);
+	for (n = 1; n <= 100; n++)
+	{
+	if (n % 3 == 0 && n % 5 == 0)
+	{
+		printf("%s", "FizzBuzz");
+	}
+	else if (n % 3 == 0)
+	{
+		printf("%s", "Fizz");
+	}
+	else if (n % 5 == 0)
+	{
+		printf("%s", "Buzz");
+	}
+	else
+	{
+		printf("%i", n);
+	}
+	if (n != 100)
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
 }

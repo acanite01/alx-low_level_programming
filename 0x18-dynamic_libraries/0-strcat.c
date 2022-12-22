6d6d35bef42ae938abeb05098361108ec2f812a3
acanite01 <107746972+acanite01@users.noreply.github.com>
2022-12-22 23:25:35 +0100

    task

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two stings
- * @dest: Destination string
- * @src: the source of the string
- * Return: Returns a pointer to the resulting string dest
+ * *_strcat - function comute strings
+ *@dest: pointing to a char
+ *@src: pointing to a char
+ *Return: value of dest
  */
 
 char *_strcat(char *dest, char *src)
@@ -13,17 +13,20 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
+	j = 0;
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
 		j++;
+		i++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }

6173390769f79642181ffe508fbdb2a720384d12
acanite01 <107746972+acanite01@users.noreply.github.com>
2022-12-22 23:31:01 +0100

    task

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -1,22 +1,23 @@
 #include "main.h"
+
 /**
-*_strchr - Returns a pointer to the first occurrence
-*of the character c in the string s, or NULL if the
-*character is not found
-*
-*@s:string targeted
-*@c:character targeted
-*
-*Return: returns pointer to first occcurence of c
-*/
+ * _strchr - locates character in string
+ * @s: string
+ * @c: Character to search
+ * Return: Pointer to first instance of character
+ *
+ */
 char *_strchr(char *s, char c)
 {
-	int i;
+	while (*s)
+	{
+		s++;
 
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+
+	return (0);
 }
